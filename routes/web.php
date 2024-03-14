e4243df64c5ffefe7a7c2ac19e3e8a1ede554df1
Jerald David J. Cane <cane.jeralddavidj@gmail.com>
2024-03-14 16:11:09 +0800

    removed create page and replaced with create modal, removed web route for create page, removed create function in product controller

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
 Route::get('/products/{products}/edit', [ProductController::class, 'edit'])->name('products.edit');
